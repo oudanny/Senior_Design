16ab3b02642460f70718bf28661fcaae33c57409
Daniel McAllister-Ou <135235559+oudanny@users.noreply.github.com>
2026-02-16 17:09:38 +0000

    Change prod data to daily rates for CMG

---
+++ forecasting/conv_monthly_rate_to_daily_rate.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+monthly_rate_df = pd.read_csv('forecasting/norm_monthly_rates.csv')
+
+# Find days in each month for each prod_date
+monthly_rate_df['prod_date'] = pd.to_datetime(monthly_rate_df['prod_date'])
+monthly_rate_df['days_in_month'] = monthly_rate_df['prod_date'].dt.days_in_month
+# Convert monthly rates to daily rates
+monthly_rate_df['Oil_rate'] = monthly_rate_df['Oil_rate'] / monthly_rate_df['days_in_month']
+monthly_rate_df['Gas_rate'] = monthly_rate_df['Gas_rate'] / monthly_rate_df['days_in_month']
+monthly_rate_df['Water_rate'] = monthly_rate_df['Water_rate'] / monthly_rate_df['days_in_month']
+# Drop the 'days_in_month' column as it's no longer needed
+monthly_rate_df = monthly_rate_df.drop(columns=['days_in_month'])
+# Save the updated dataframe
+monthly_rate_df.to_csv('forecasting/norm_daily_rates.csv', index=False)
+print("Monthly rates have been converted to daily rates and saved to 'forecasting/norm_daily_rates.csv'.")
--- forecasting/prepare_csv_for_CMG_HM.py
+++ forecasting/prepare_csv_for_CMG_HM.py
@@ -1,7 +1,9 @@
 import pandas as pd 
 import re
 
-csv = pd.read_csv('forecasting/monthly_cumulative_volumes.csv')
+# csv = pd.read_csv('forecasting/monthly_cumulative_volumes.csv')
+# csv = pd.read_csv('forecasting/norm_monthly_rates.csv')
+csv = pd.read_csv('forecasting/norm_daily_rates.csv')
 # =================================================
 
 def extract_code(s):
@@ -12,8 +14,10 @@ def extract_code(s):
 
 csv['WellName'] = csv['WellName'].apply(extract_code)
 csv = csv.rename(columns={'WellName': 'Well'})
-new_row = {"Well": 'Name', "prod_date": 'YYYY-MM-DD', "Oil": 'Bbl/Mo', "Gas": 'MCF/Mo', "Water": 'BBl/Mo'}
-# HM_csv = pd.concat([pd.DataFrame([new_row]), csv], ignore_index=True)
+# new_row = {"Well": 'Name', "prod_date": 'YYYY-MM-DD', "Oil_rate": 'Bbl/Mo', "Gas_rate": 'MCF/Mo', "Water_rate": 'BBl/Mo'}
+new_row = {"Well": 'Name', "prod_date": 'YYYY-MM-DD', "Oil_rate": 'STB/d', "Gas_rate": 'MCF/d', "Water_rate": 'STB/d'}
+
+HM_csv = pd.concat([pd.DataFrame([new_row]), csv], ignore_index=True)
 
 
-csv.to_csv('forecasting/HM_norm_monthly_cum.prd', index=False)
+HM_csv.to_csv('forecasting/HM_norm_daily_rates.prd', index=False)
